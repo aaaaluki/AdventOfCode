3f65b03ebacc951ac1be8a90fb67af1106dd0ca4
aaaaluki <luki00.lrb@gmail.com>
2022-12-02 12:05:42 +0100

    Updated mkadvent.sh

---
+++ mkadvent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+[ $# -ne 0 ] && echo -e "$ $0" && exit 1
+
+day=$(date +%d)
+year=$(date +%Y)
+folder="${year}/${day}"
+
+mkdir -p $folder
+echo "Created ${folder}"
+
+cp day.py "${folder}/day${day}.py"
+touch "${folder}/test"
+curl --cookie cookies.txt "https://adventofcode.com/2022/day/${day#0}/input" > "${folder}/input"
